61629a1e4ce4cec69db2fea31d1b1db17aa5776c
Daniel <daniel.lam@8x8.com>
2019-10-20 14:00:00 +0100

    first commit

---
+++ src/pages/contact.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../components/layout"
+export default () => (
+  <Layout>
+    <div>
+      <h1>I'd love to talk! Email me at the address below</h1>
+      <p>
+        <a href="mailto:daniel.lam@8x8.com">daniel.lam@8x8.com</a>
+      </p>
+    </div>
+  </Layout>
+)
